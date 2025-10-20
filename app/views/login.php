8a8d855a5cbf603dad7e0df585bca2ef7d5bc6f6
lithmalperera <lithmal.perera2002@gmail.com>
2025-10-20 15:08:19 +0530

    Refactor AuthController and User model; enhance user registration and login processes
    - Updated AuthController to handle user registration and login with improved error handling and validation.
    - Introduced new User model methods for user creation, retrieval, and updates.
    - Removed UserModel.php as its functionality is now integrated into the User model.
    - Enhanced user input validation for registration and profile updates.
    - Updated views for signup, login, and profile to reflect new structure and improve user experience.
    - Adjusted CSS for better styling and responsiveness across forms.

--- app/views/login.php
+++ app/views/login.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome to Hanthana</title>
-    <link rel="stylesheet" href="../../public/css/general.css">
-    <link rel="stylesheet" href="../../public/css/login.css">
-    <link rel="stylesheet" href="../../public/css/notificationpopup.css">
+    <link rel="stylesheet" href="public/css/general.css">
+    <link rel="stylesheet" href="public/css/login.css">
+    <link rel="stylesheet" href="public/css/notificationpopup.css">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
 </head>
 <body>
@@ -28,7 +28,7 @@
         </div>
     </div>
 
-    <script src="../../public/js/login.js"></script>
+    <script src="public/js/login.js"></script>
 </body>
 </html>
 
